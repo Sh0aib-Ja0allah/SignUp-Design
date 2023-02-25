85c7f504a8a79fe972afe367878b9c4d197fd914
Che S Jadallah <110256562+Sh0aib-Ja0allah@users.noreply.github.com>
2023-02-25 02:43:50 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Child from "./Components/Child/Child.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Child />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
